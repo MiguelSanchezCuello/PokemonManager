9ef04a5d89bc7b4d8ea0b7c5d9be3979aa4c9f18
MIGUEL SANCHEZ <miguelsc10@hotmail.com>
2025-12-16 18:05:55 -0400

    Primer comit capitulo 8

---
+++ src/app/componentes/generaciones/generaciones.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-generaciones',
+  imports: [],
+  templateUrl: './generaciones.html',
+  styleUrl: './generaciones.css',
+})
+export class Generaciones {
+
+}
