f4e26a47d88e105426640582be7c84e33a23d198
MIGUEL SANCHEZ <miguelsc10@hotmail.com>
2025-12-22 16:41:43 -0400

    completada la practica del capitulo 9

--- src/app/componentes/generaciones/generaciones.ts
+++ src/app/componentes/generaciones/generaciones.ts
@@ -1,11 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-generaciones',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './generaciones.html',
   styleUrl: './generaciones.css',
 })
-export class Generaciones {
 
+export class Generaciones implements OnInit {
+  generaciones = [
+    "Generacion I",
+    "Generacion II",
+    "Generacion III",
+    "Generacion IV",
+    "Generacion V",
+    "Generacion VI",
+    "Generacion VII",
+  ]
+
+  constructor() {}
+
+  ngOnInit() {
+    console.log('Componente Generaciones cargado');
+  }
 }
