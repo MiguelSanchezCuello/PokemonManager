bab29abf197693521509fa3b49402b619ca23fec
MIGUEL SANCHEZ <miguelsc10@hotmail.com>
2025-12-23 22:35:10 -0400

    completada la practica del capitulo 10

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,11 +1,16 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { LOCALE_ID } from '@angular/core';
+import { registerLocaleData } from '@angular/common';
+import es from '@angular/common/locales/es';
+registerLocaleData(es);
 
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes)
+    provideRouter(routes),
+    { provide: LOCALE_ID, useValue: 'es' }
   ]
 };
--- src/app/componentes/pokemons/pokemons.ts
+++ src/app/componentes/pokemons/pokemons.ts
@@ -1,19 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-pokemons',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './pokemons.html',
   styleUrl: './pokemons.css',
 })
-export class Pokemons {
-  unaCadenaDeCaracteres = "Una variable a mostrar";
-  unNumero = 42;
-  unObjeto = { apellido: "Wayne", nombre: "Bruce", ciudad: "Gotham"};
+export class Pokemons implements OnInit {
+  
+  pokemons = [
+    { 'id': 1, 'nombre': 'Bulbasaur', 'img': 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png', 'categoria': 'semilla', 'tipo': ['planta', 'veneno'], 'talla': 0.70, 'peso': 6.9 },
+    { 'id': 2, 'nombre': 'Ivysaur', 'img': 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/2.png', 'categoria': 'semilla', 'tipo': ['planta', 'veneno'], 'talla': 1.00, 'peso': 13.0 },
+    { 'id': 3, 'nombre': 'Venusaur', 'img': 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/3.png', 'categoria': 'semilla', 'tipo': ['planta', 'veneno'], 'talla': 2.00, 'peso': 100.0 },
+    { 'id': 4, 'nombre': 'Charmander', 'img': 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/4.png', 'categoria': 'lagarto', 'tipo': ['fuego'], 'talla': 0.60, 'peso': 8.5 },
+    { 'id': 5, 'nombre': 'Charmeleon', 'img': 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/5.png', 'categoria': 'llama', 'tipo': ['fuego'], 'talla': 1.10, 'peso': 19.0 },
+    { 'id': 6, 'nombre': 'Charizard', 'img': 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/6.png', 'categoria': 'llama', 'tipo': ['fuego', 'volador'], 'talla': 1.70, 'peso': 90.5 },
+  ];
 
   constructor() {}
 
-  onInit() {
+  ngOnInit() {
     console.log('Componente Pokemons cargado');
   }
 } 
