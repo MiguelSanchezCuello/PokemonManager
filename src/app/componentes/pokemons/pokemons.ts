2aeda67bbbfec6d41dc75e48e27cab19076deac3
MIGUEL SANCHEZ <miguelsc10@hotmail.com>
2025-12-27 23:39:13 -0400

    Creacion de un servicio para manejar la lista de pokemons

--- src/app/componentes/pokemons/pokemons.ts
+++ src/app/componentes/pokemons/pokemons.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Pokemon } from '../pokemon/pokemon';
+import { Pokemon as PokemonService } from '../../services/pokemon';
 
 @Component({
   selector: 'app-pokemons',
@@ -12,18 +13,22 @@ export class Pokemons implements OnInit {
   
   pokemonSeleccionado: any;
 
-  pokemons = [
-    { 'id': 1, 'nombre': 'Bulbasaur', 'img': 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png', 'categoria': 'semilla', 'tipo': ['planta', 'veneno'], 'talla': 0.70, 'peso': 6.9 },
-    { 'id': 2, 'nombre': 'Ivysaur', 'img': 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/2.png', 'categoria': 'semilla', 'tipo': ['planta', 'veneno'], 'talla': 1.00, 'peso': 13.0 },
-    { 'id': 3, 'nombre': 'Venusaur', 'img': 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/3.png', 'categoria': 'semilla', 'tipo': ['planta', 'veneno'], 'talla': 2.00, 'peso': 100.0 },
-    { 'id': 4, 'nombre': 'Charmander', 'img': 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/4.png', 'categoria': 'lagarto', 'tipo': ['fuego'], 'talla': 0.60, 'peso': 8.5 },
-    { 'id': 5, 'nombre': 'Charmeleon', 'img': 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/5.png', 'categoria': 'llama', 'tipo': ['fuego'], 'talla': 1.10, 'peso': 19.0 },
-    { 'id': 6, 'nombre': 'Charizard', 'img': 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/6.png', 'categoria': 'llama', 'tipo': ['fuego', 'volador'], 'talla': 1.70, 'peso': 90.5 },
-  ];
+  pokemons = [];
+  cuenta = 0;
 
-  constructor() {}
+  // pokemons = [
+  //   { 'id': 1, 'nombre': 'Bulbasaur', 'img': 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png', 'categoria': 'semilla', 'tipo': ['planta', 'veneno'], 'talla': 0.70, 'peso': 6.9 },
+  //   { 'id': 2, 'nombre': 'Ivysaur', 'img': 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/2.png', 'categoria': 'semilla', 'tipo': ['planta', 'veneno'], 'talla': 1.00, 'peso': 13.0 },
+  //   { 'id': 3, 'nombre': 'Venusaur', 'img': 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/3.png', 'categoria': 'semilla', 'tipo': ['planta', 'veneno'], 'talla': 2.00, 'peso': 100.0 },
+  //   { 'id': 4, 'nombre': 'Charmander', 'img': 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/4.png', 'categoria': 'lagarto', 'tipo': ['fuego'], 'talla': 0.60, 'peso': 8.5 },
+  //   { 'id': 5, 'nombre': 'Charmeleon', 'img': 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/5.png', 'categoria': 'llama', 'tipo': ['fuego'], 'talla': 1.10, 'peso': 19.0 },
+  //   { 'id': 6, 'nombre': 'Charizard', 'img': 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/6.png', 'categoria': 'llama', 'tipo': ['fuego', 'volador'], 'talla': 1.70, 'peso': 90.5 },
+  // ];
+
+  constructor(private servicePokemons: PokemonService) {}
 
   ngOnInit() {
-    console.log('Componente Pokemons cargado');
+    this.pokemons = this.servicePokemons.getPokemons();
+    this.cuenta = this.servicePokemons.contandoPokemons();
   }
 } 
